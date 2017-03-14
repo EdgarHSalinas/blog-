d614667bdefebc87566eab851629f5494191168b
Edgar H Salinas <edgar@my.codergv.org>
2017-03-14 11:04:16 -0700

    Up to 13
    CRUD views setup

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,18 @@
-from django.http import HTTPResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def posts_home(request):
-    return HTTPResponse("<h1>Hello<h1>")
+def post_create(request):
+    return HttpResponse("<h1>Create<h1>")
+
+def post_detail(request): # READ or retrieve 
+    return HttpResponse("<h1>Detail<h1>")
+
+def post_list(request): # list items 
+    return HttpResponse("<h1>List<h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>Update<h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete<h1>")
